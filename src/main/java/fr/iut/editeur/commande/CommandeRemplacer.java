809da241b1880a7e1f0d039cab1bc4ac25d9f284
Guilhot Enzo <enzo.guilhot@etu.umontpellier.fr>
2023-11-08 08:58:47 +0100

    methode remplacer de Document
    ajout classe CommandeRemplacer
    
    maj createCommand dans CommandeFactory
    
    ajout fonctionnalite remplacer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,25 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;indexDebut;indexFin;texte");
+            return;
+        }
+        int indexDebut = Integer.parseInt(parameters[1]);
+        int indexFin = Integer.parseInt(parameters[2]);
+        String texte = parameters[3];
+        this.document.remplacer(indexDebut, indexFin, texte);
+        super.executer();
+    }
+
+}
